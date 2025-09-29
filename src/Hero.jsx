2a21d1167df7319e14a269e3fb2cf922f486195f
itsromen <ugtromen449@gmail.com>
2025-09-30 00:00:10 +0300

    Improve responsiveness for first section

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,21 +2,21 @@ import NavBar from "./NavBar";
 
 function Hero() {
   return (
-    <header className="relative h-dvh flex flex-col bg-[#222831] text-[#EEEEEE] overflow-clip ">
+    <header className="relative lg:h-dvh flex flex-col bg-[#222831] text-[#EEEEEE] overflow-clip ">
       <NavBar />
-      <div className="w-screen relative self-center max-w-[2000px] flex-auto grid grid-cols-2 place-items-center">
-        <div className="w-full">
-          <div className="border absolute border-[#EEEEEE1A] left-[436px] h-screen bottom-0" />
-          <div className="relative pl-32 pb-24 flex flex-col gap-12">
+      <div className="py-12 lg:py-0 w-screen relative self-center max-w-[2000px] flex-auto grid lg:grid-cols-2 place-items-center">
+        <div className="lg:w-full">
+          <div className="hidden lg:block border absolute border-[#EEEEEE1A] left-[358px] min-[1130px]:left-[436px] h-screen bottom-0" />
+          <div className="relative text-center sm:text-start sm:pl-32 pb-24 flex flex-col gap-12">
             <img
               src="arrow-doodle.svg"
               alt="Arrow"
-              className="absolute left-10 top-10"
+              className="hidden sm:inline absolute left-10 top-12"
             />
-            <h1 className="text-8xl font-bold max-w-[600px]">
+            <h1 className="text-5xl sm:text-7xl min-[1384px]:text-8xl font-bold max-w-[600px]">
               CREATIVE UI <span className="text-[#00ADB5]">DESIGNER</span>
             </h1>
-            <div className="flex gap-6">
+            <div className="justify-center sm:justify-start flex gap-6">
               <button className="bg-[#00ADB5] py-2.5 px-8 rounded-3xl font-bold">
                 Hire Me
               </button>
@@ -25,11 +25,11 @@ function Hero() {
               </button>
             </div>
           </div>
-          <div className="w-24 h-24 bg-[#393E46BF] grid place-items-center absolute bottom-16 left-[388px]">
+          <div className="hidden w-18 h-18 min-[1384px]:w-24 min-[1384px]:h-24 bg-[#393E46BF] md:grid place-items-center absolute right-24 max-lg:top-55 lg:bottom-8 min-lg:max-[1130px]:left-[322px] min-[1130px]:left-[400px] min-[1384px]:bottom-16 min-[1384px]:left-[388px]">
             <img src="down-arrow.svg" alt="Arrow pointing down" />
           </div>
         </div>
-        <div className="relative">
+        <div className="relative max-sm:scale-80">
           <img src="hero-doodles.svg" alt="Doodles" />
           <img src="shadow.svg" alt="Shadow" className="absolute -bottom-2" />
           <img src="dude.svg" alt="Man" className="absolute -bottom-24" />
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,8 +1,8 @@
 function NavBar() {
   return (
-    <nav className="flex justify-between items-center px-24 py-12 border-b border-[#EEEEEE1A]">
+    <nav className="flex justify-between items-center px-10 py-6 md:px-24 md:py-12 border-b border-[#EEEEEE1A]">
       <h3 className="text-2xl font-bold">SaulDesign</h3>
-      <ul className="flex text-lg gap-12">
+      <ul className="hidden md:flex text-lg gap-12">
         <li>
           <a href="#">Home</a>
         </li>
@@ -13,6 +13,9 @@ function NavBar() {
           <a href="#">Contact</a>
         </li>
       </ul>
+      <button className="md:hidden">
+        <img src="menu.svg" alt="Menu Button" className="w-10" />
+      </button>
     </nav>
   );
 }
